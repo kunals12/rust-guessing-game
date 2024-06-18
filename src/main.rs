76bd087bd436e81187bb7d1f1756bad002776fc5
kunals12 <work.kunal12@gmail.com>
2024-06-18 09:42:19 +0530

    feat: random number generator

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+    let secret_number:i8 = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess.");
 
     let mut guess = String::new(); // mutable variable that is currently bound to a new, empty instance of a String
